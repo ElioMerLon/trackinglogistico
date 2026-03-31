f9f337c88c704da514e161fcf3c115be4b13cb8f
Merlon04 <elio.merlon04@gmail.com>
2026-03-30 22:49:45 -0500

    inicio

---
+++ trackinglogistico/src/main/java/com/logistica/trackinglogistico/TrackinglogisticoApplication.java
@@ -0,0 +1,13 @@
+package com.logistica.trackinglogistico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackinglogisticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackinglogisticoApplication.class, args);
+	}
+
+}
---
+++ trackinglogistico/src/test/java/com/logistica/trackinglogistico/TrackinglogisticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistica.trackinglogistico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackinglogisticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
