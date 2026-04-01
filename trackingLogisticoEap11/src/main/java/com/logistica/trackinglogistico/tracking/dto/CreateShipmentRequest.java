be105b5f405b8b54d0406bd1e2400cdf25d6a90b
Merlon04 <elio.merlon04@gmail.com>
2026-04-01 01:00:03 -0500

    notNull

--- trackingLogisticoEap11/src/main/java/com/logistica/trackinglogistico/tracking/dto/CreateShipmentRequest.java
+++ trackingLogisticoEap11/src/main/java/com/logistica/trackinglogistico/tracking/dto/CreateShipmentRequest.java
@@ -1,13 +1,14 @@
 package com.logistica.trackinglogistico.tracking.dto;
 
-import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.NotNull;
 
 public class CreateShipmentRequest {
 
-    @NotBlank
+    @NotNull
     private int idOperator;
 
-    @NotBlank
+    @NotNull
     private int idPackage;
 
     public CreateShipmentRequest() {
